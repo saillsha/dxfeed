abce733a92f9d8dbb92a664dd36a9ff5a43d17e8
saillsha <sahilshah41@gmail.com>
2015-07-20 09:14:54 -0400

    include first trade's sequence number in the cluster file

--- src/quotail/Cluster.java
+++ src/quotail/Cluster.java
@@ -109,6 +109,8 @@ public class Cluster {
 		sb.append(",\"time\":");
 		TimeAndSale tns = this.trades.get(0);
 		sb.append(tns.getTime());
+		sb.append(",\"sequence\":");
+		sb.append(tns.getSequence());
 		sb.append(",\"creationTime\":");
 		sb.append(this.creationTime);
 		sb.append(",\"bid\":");
